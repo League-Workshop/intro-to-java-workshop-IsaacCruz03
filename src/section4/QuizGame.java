78cd000a06003f81f5d0a300e0d3b76162e7aaa7
League Student <league@iMac-15.attlocal.net>
2019-07-18 14:25:28 -0700

    Day 4

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -9,14 +9,16 @@ public class QuizGame {
 		// 1.  Create a variable to hold the user's score 
 		int score=0;
 		// 2.  Ask the user a question 
-
+String input=JOptionPane.showInputDialog("What is Obama's last name?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if (input.equals("Obama")) {
 		// 4.  if the user's answer was correct, add one to their score 
-		
+			
+			}
+		}
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
 		
 		// 6.  After all the questions have been asked, print the user's score 
 		
 	}
-}
+
--- src/section4/RobotTreasureHunt.java
+++ src/section4/RobotTreasureHunt.java
@@ -16,25 +16,28 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotTreasureHunt implements KeyEventDispatcher{
 
 	// 1. Create a new mini robot (type "mini" inside the parentheses)
-	
+	Robot rob=new Robot("mini");
 	private void goUp() throws InterruptedException {
 		// 2. Make the robot move up the screen (use setAngle(angle) and microMove(distance))
-		
-	}
+		rob.move(50);
+		rob.setAngle(45);
+		}
 
 	private void goDown() throws InterruptedException{
 		// 3. make the robot move down the screen (use setAngle(angle) and microMove(distance))
-		
+		rob.setAngle(-90);
+		rob.move(50);
 	}
 
 	private void turnLeft() throws InterruptedException{
 		// 4. Make the robot turn to the left (use setAngle(angle) and microMove(distance))
-
+rob.setAngle(70);
+rob.move(-50);
 	}
 
 	private void turnRight() throws InterruptedException{
 		// 5. make the robot turn to the right (use setAngle(angle) and microMove(distance))
-		
+		rob.turn(50);
 	}
 
 	private void spaceBarWasPressed() {
